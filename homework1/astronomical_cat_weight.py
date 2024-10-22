a781519f208f726fc44e558f1e7c6b5b318b5f00
Christian Figueroa <chrjfigueroa@gmail.com>
2024-10-22 00:28:35 -0700

    removed hw1

--- homework1/astronomical_cat_weight.py
+++
@@ -1,6 +0,0 @@
-Cat = 4.5 #How much my cat weighs
-Solar_conversion = 1/(1.99*(10**30)) #Conversion from kg to AU
-
-print(Cat*Solar_conversion) #My cat weighs 2.28x10^-30 Solar Masses!
-
-#I would run this command with "sun_cat()" 
